d454f2d512bb101b18a1d1eacf3b0ea2eca7dd3d
Brian <briandsalemi@gmail.com>
2020-10-12 08:53:12 -0400

    Added bootstrap jumbotron to QuestionBox component and added a slow for the question, p tag for the answers, and buttons to submit an answer and move to the next question

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Question Box</h1>
+    <div class="question-box-container">
+      <b-jumbotron >
+
+        <template v-slot:lead>
+          Question
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          List of Answers
+        </p>
+
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button variant="success" href="#">Next Question </b-button>
+      </b-jumbotron>
+  </div>
 </template>
