0e6031460adf7776892db3037cf50b82e40c1061
Brian <briandsalemi@gmail.com>
2020-10-15 09:03:07 -0400

    Added a v-if check to App.vue to only load the QuestionBox component if the promise has finished fetching the questions from the api. Created the answers function in QuestionBox component which combines the incorrect and correct answers for the questions into one array. Displayed the answers with a v-for loop within a p tag in QuestionBox component

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,8 +8,8 @@
 
         <hr class="my-4">
 
-        <p>
-          List of Answers
+        <p v-for="(answer, index) in answers" :key="index">
+          {{answer}}
         </p>
 
         <b-button variant="primary" href="#">Submit</b-button>
@@ -23,6 +23,14 @@
     props: {
       currentQuestion: Object,
       nextQuestion: Function
+    },
+    computed: {
+      answers(){
+        let answers = [...this.currentQuestion.incorrect_answers]
+        answers.push(this.currentQuestion.correct_answer)
+
+        return answers
+      }
     }
   }
 </script>
