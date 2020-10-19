a9370f5934a5141c94a9c834e3aec013b0d02c87
Brian <briandsalemi@gmail.com>
2020-10-19 09:28:12 -0400

    Added a bootstrap list component and moved the answers for loop inside of it. Added some margins to the .list-group and .btn classes

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,9 +8,14 @@
 
         <hr class="my-4">
 
-        <p v-for="(answer, index) in answers" :key="index">
-          {{answer}}
-        </p>
+        <b-list-group>
+          <b-list-group-item 
+            v-for="(answer, index) in answers" 
+            :key="index"
+          >
+             {{answer}}
+          </b-list-group-item>
+        </b-list-group>
 
         <b-button variant="primary" href="#">Submit</b-button>
         <b-button @click="nextQuestion" variant="success" href="#">Next Question </b-button>
@@ -34,3 +39,13 @@
     }
   }
 </script>
+
+<style scoped>
+  .list-group{
+    margin-bottom: 15px;
+  }
+
+  .btn {
+    margin: 0 5px;
+  }
+</style>
